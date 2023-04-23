e99ae318ffe8b8fe6df11dc2579950d375d3327d
wequbles <120826725+wequbles@users.noreply.github.com>
2023-04-23 20:47:39 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@ $(document).ready(function() {
 		return false;
 	});
 });
+
+function openCity(evt, tabName) {
+  var i, store__tab, store__menu__btn;
+  store__tab = document.getElementsByClassName("store__tab");
+  for (i = 0; i < store__tab.length; i++) {
+    store__tab[i].style.display = "none";
+  }
+  store__menu__btn = document.getElementsByClassName("store__menu__btn");
+  for (i = 0; i < store__menu__btn.length; i++) {
+    store__menu__btn[i].className = store__menu__btn[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
